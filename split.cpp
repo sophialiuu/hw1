bcb19e4d923852c7d5e20efc6346ad18b6b2ae62
Sophia Liu <sophialiurose@gmail.com>
2023-01-19 15:42:34 -0800

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,34 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#define nullptr 0
+#include <cstdlib>
 
 /* Add a prototype for a helper function here if you need */
 
+//assume list is already sorted 
+
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  //base case: list is empty/at end of list, return 
+  if(in == nullptr){
+    return;
+  }
+  else{
+    split(in->next,odds,evens);
+    //otherwise, if element is even, add it to even list 
+    if(in->value % 2 == 0){
+      in->next = evens; //connect previous evens link to current pointer 
+      evens = in;  //update evens to even pointer 
+      in = nullptr;
+    }
+    //otherwise, if element is odd, add it to odds list 
+    else if(in->value % 2 != 0){
+      in->next = odds; //connect previous odds link to current pointer 
+      odds = in; //update odds to current pointer 
+      in = nullptr;
+    } 
+  }
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,63 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+
 
 int main(int argc, char* argv[])
 {
+Node * node = new Node(1,NULL);
+    node->next = new Node(2,NULL);
+    node->next->next = new Node(5,NULL);
+    node->next->next->next = new Node(4,NULL);
+    node->next->next->next->next = new Node(5,NULL);
+
+    Node * odds = NULL;
+    Node * evens = NULL;
+
+    Node * firstEven = evens;
+    Node * firstOdd = odds;
+
+    
+    split(node, odds, evens);
+
+    std::cout << "odd list after splitting input list: ";
+    Node * temp = odds;
+    while(temp)
+    {
+        std::cout << temp->value << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+
+    std::cout << "even list after splitting input list: ";
+    temp = evens;
+    while(temp)
+    {
+        std::cout << temp->value << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
+
+    while(evens)
+    {
+        Node * temp = evens->next;
+        delete evens;
+        evens = temp;
+    }
+    std::cout << std::endl;
+    while(odds)
+    {
+        Node * temp = odds->next;
+        delete odds;
+        odds = temp;
+    }
+    std::cout << std::endl;
+
+
+    return 0;
+
 
 }
